06ed597c5a982202ebcc7e454a1b5d6c1c68e080
Pradhyumna N Holla <pradholla@gmail.com>
2022-10-20 18:42:31 +0530

    Minor Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,31 @@
-#Text summarization app
 import streamlit as st
 from transformers import pipeline
+from summarizer import Summarizer, TransformerSummarizer
 
 st.set_page_config(page_title="Text Summarizer" ,layout="wide", page_icon=":books:")
 st.header("Text Summarizer")
 
-# Creating a summarizer object
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+st.sidebar.header("Summarizer")
+st.sidebar.subheader("Select the summarizer you want to use")
+summarizer = st.sidebar.selectbox("Summarizer", ("XLNet Summarizer", "BERT Summarizer","GPT 2","Hugging Face"))
 
-# Creating a text input box
 text = st.text_area("Enter Text Here")
 
-# Creating a button
 if st.button("Summarize"):
-    summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
-    st.write(summary[0]['summary_text'])
+    if summarizer == "XLNet Summarizer":
+        model = TransformerSummarizer(transformer_type="XLNet",transformer_model_key="xlnet-base-cased")
+        summary = model(text)
+
+    elif summarizer == "BERT Summarizer":
+        model = Summarizer()
+        summary = model(text)
+
+    elif summarizer == "GPT 2":
+        GPT2_model = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
+        summary = ''.join(GPT2_model(text, min_length=60))
+        
+    elif summarizer == "Hugging Face":
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        summary = summarizer(text, max_length=200, min_length=200, do_sample=False)
+        summary = summary[0]['summary_text']
+    st.write(summary)
