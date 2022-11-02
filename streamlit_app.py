5abe64c62d9fe6819bf628ba52e6de4df53ce84f
Pradhyumna N Holla <pradholla@gmail.com>
2022-11-02 10:15:43 +0530

    Two models

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,23 +7,17 @@ st.header("Text Summarizer")
 
 st.sidebar.header("Summarizer")
 st.sidebar.subheader("Select the summarizer you want to use")
-summarizer = st.sidebar.selectbox("Summarizer", ("XLNet Summarizer", "BERT Summarizer","GPT 2","Hugging Face"))
+summarizer = st.sidebar.selectbox("Summarizer", ("t5-base","Hugging Face"))
 
 text = st.text_area("Enter Text Here")
 
 if st.button("Summarize"):
-    if summarizer == "XLNet Summarizer":
-        model = TransformerSummarizer(transformer_type="XLNet",transformer_model_key="xlnet-base-cased")
-        summary = model(text)
+    if summarizer == "t5-base":
+        GPT2_model = pipeline("summarization", model="santiviquez/ssr-base-finetuned-samsum-en")
+        summary = GPT2_model(text, max_length=200, min_length=200, do_sample=False)
+        summary = summary[0]['summary_text']
 
-    elif summarizer == "BERT Summarizer":
-        model = Summarizer()
-        summary = model(text)
 
-    elif summarizer == "GPT 2":
-        GPT2_model = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
-        summary = ''.join(GPT2_model(text, min_length=60))
-        
     elif summarizer == "Hugging Face":
         summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
         summary = summarizer(text, max_length=200, min_length=200, do_sample=False)
