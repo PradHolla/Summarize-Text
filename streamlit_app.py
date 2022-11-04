8d97e8d7a61f7ec8d13139f90cce5cdd1625393f
PradHolla <pradholla@gmail.com>
2022-11-04 11:08:01 +0530

    Finalize the model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,14 @@
 import streamlit as st
 from transformers import pipeline
-from summarizer import Summarizer, TransformerSummarizer
 
 st.set_page_config(page_title="Text Summarizer" ,layout="wide", page_icon=":books:")
 st.header("Text Summarizer")
-
-st.sidebar.header("Summarizer")
-st.sidebar.subheader("Select the summarizer you want to use")
-summarizer = st.sidebar.selectbox("Summarizer", ("t5-base","Hugging Face"))
+st.subheader("Summarize your text in a few clicks")
 
 text = st.text_area("Enter Text Here")
 
 if st.button("Summarize"):
-    if summarizer == "t5-base":
-        GPT2_model = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-        summary = GPT2_model(text, max_length=400, min_length=250, do_sample=False)
-        summary = summary[0]['summary_text']
-
-
-    elif summarizer == "Hugging Face":
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-        summary = summarizer(text, max_length=400, min_length=250, do_sample=False)
-        summary = summary[0]['summary_text']
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    summary = summarizer(text, max_length=400, min_length=250, do_sample=False)
+    summary = summary[0]['summary_text']
     st.write(summary)
